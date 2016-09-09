053dd27a8f23cf22daff4b8dbc1eda15dd2455c9
Patrick Spek <p.spek@tyil.nl>
2016-09-09 13:44:18 +0200

    Implement AgeCalculator

---
+++ Calculator/AgeCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class AgeCalculator : IAgeCalculator
+    {
+        string[] formats = {
+                               "dd-MM-yyyy"
+                           };
+        string culture = "nl-NL";
+
+        public bool ParseInput(string input, out DateTime result)
+        {
+            return DateTime.TryParseExact(
+                input,
+                this.formats,
+                new CultureInfo(this.culture),
+                DateTimeStyles.None,
+                out result
+            );
+        }
+
+        public int CalculateAge(DateTime dob)
+        {
+            DateTime now = DateTime.Now;
+            int years = now.Year - dob.Year;
+
+            Console.WriteLine("{0} < {1}", now.Month, dob.Month);
+            if ((now.Month < dob.Month) || (now.Month == dob.Month && now.Day < dob.Day))
+            {
+                years--;
+            }
+
+            return years;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Calculator
@@ -10,8 +11,31 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Success!");
+            AgeCalculator c = new AgeCalculator();
+            DateTime dt;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a valid date: ");
+                    string input = Console.ReadLine();
+
+                    if (c.ParseInput(input, out dt)) {
+                        break;
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Incorrect date format. Use dd-mm-yyyy and a VALID date.");
+                }
+            }
+
+            Console.WriteLine("You are {0} years old!", c.CalculateAge(dt));
+
+            // let's not close the console immediatly
             Console.ReadLine();
         }
+
     }
 }
